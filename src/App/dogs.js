2ee567890c29eceb4cd10230a7f7e8bc42ec8ba6
Heath JH Moore <heathjhmoore@gmail.com>
2019-06-19 18:45:11 -0500

    setup

---
+++ src/App/dogs.js
@@ -0,0 +1,49 @@
+const dogs = [
+  {
+    id: 'dog1',
+    name:'billy',
+    disposition: 'rude',
+    imageUrl: 'https://i.ytimg.com/vi/Gw_xvtWJ6q0/hqdefault.jpg',
+    fullness: 67,
+    favPose: 'downward facing dog',
+    color: 'white',
+  },
+  {
+    id: 'dog2',
+    name:'bobby',
+    disposition: 'nice',
+    imageUrl: 'https://i.ytimg.com/vi/Gw_xvtWJ6q0/hqdefault.jpg',
+    fullness: 67,
+    favPose: 'warrior',
+    color: 'white',
+  },
+  {
+    id: 'dog3',
+    name:'barney',
+    disposition: 'pugnacious',
+    imageUrl: 'https://i.ytimg.com/vi/Gw_xvtWJ6q0/hqdefault.jpg',
+    fullness: 67,
+    favPose: 'downward facing dog',
+    color: 'white',
+  },
+  {
+    id: 'dog4',
+    name:'gruff',
+    disposition: 'lazy',
+    imageUrl: 'https://i.ytimg.com/vi/Gw_xvtWJ6q0/hqdefault.jpg',
+    fullness: 67,
+    favPose: 'downward facing dog',
+    color: 'white',
+  },
+  {
+    id: 'dog5',
+    name:'belinda',
+    disposition: 'rude',
+    imageUrl: 'https://i.ytimg.com/vi/Gw_xvtWJ6q0/hqdefault.jpg',
+    fullness: 67,
+    favPose: 'downward facing dog',
+    color: 'white',
+  },
+]
+
+export default dogs;
